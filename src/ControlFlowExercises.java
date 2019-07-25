48ca62ecefd259bdadba4d7f765aaf2ade216ad8
Austin Ghigliotty <austin.ghigliotty@gmail.com>
2019-07-25 16:18:57 -0500

    do while loop complete

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,9 +1,9 @@
 public class ControlFlowExercises {
     public static void main(String[] args) {
-        int i = 5;
-        while (i <= 15) {
+        int i = 2;
+        do {
             System.out.println(i);
-            i++;
-        }
+            i *= i;
+        }while(i < 65537 && i > 0);
     }
 }
