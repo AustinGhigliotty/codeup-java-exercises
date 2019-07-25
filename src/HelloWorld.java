3f60ca14fc8cb192597dd9aa3c460cd015676150
Austin Ghigliotty <austin.ghigliotty@gmail.com>
2019-07-25 09:19:34 -0500

    data types and vars exercise complete

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,21 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        int myFavoriteNumber = 7;
+        System.out.println(myFavoriteNumber);
+        String myString = "Hello";
+        System.out.println(myString);
+        float myNumber = 3.14f;
+        System.out.println(myNumber);
+        int x = 5;
+        System.out.println(++x);
+        System.out.println(x);
+        int x1 = 4 + 5;
+        int x2 = 3;
+        int y2 = 4 * x2;
+        int x3 = 10;
+        int y3 = 2;
+        x3 /= y3;
+        y3 -= x3;
     }
 }
