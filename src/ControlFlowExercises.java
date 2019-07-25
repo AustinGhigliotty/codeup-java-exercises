0af9a59596154a990708f5dab8deb40cffadc9a8
Austin Ghigliotty <austin.ghigliotty@gmail.com>
2019-07-25 17:00:10 -0500

    grades exercise complete, no bonus

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -5,21 +5,27 @@ public class ControlFlowExercises {
         Scanner scan = new Scanner(System.in);
         System.out.print("Enter a number: ");
         int number = scan.nextInt();
-//        System.out.printf("number | squared | cubed");
-        System.out.println();
-        System.out.println("Here's your table!");
-        System.out.println();
-        System.out.println("number | squared | cubed");
-        System.out.println("------ | ------- | -----");
-        for (int i = 1; i <= number; i++) {
-            System.out.printf("%d      | %-8d| %d", i, i*i, i*i*i);
-//            scan.nextLine();
-            System.out.println();
+        if (number <= 100 && number > 87) {
+            System.out.println("That grade is an A");
+        } else if (number <= 87 && number > 79) {
+            System.out.println("That grade is a B");
+        } else if (number <= 79 && number > 66) {
+            System.out.println("That grade is a C");
+        } else if (number <= 66 && number > 59) {
+            System.out.println("That grade is a D");
+        } else if (number <= 59 && number > 0) {
+            System.out.println("That grade is an F");
         }
         System.out.print("Do you want to continue? (y/n)");
         String continueAsk = scan.next();
         if (continueAsk.equals("y")) {
             main(null);
         }
+
+//        A : 100 - 88
+//        B : 87 - 80
+//        C : 79 - 67
+//        D : 66 - 60
+//        F : 59 - 0
     }
 }
