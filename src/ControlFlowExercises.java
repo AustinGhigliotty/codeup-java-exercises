2e3c1d7e0124df2b43e0a6305c956e36468217fb
Austin Ghigliotty <austin.ghigliotty@gmail.com>
2019-07-25 16:51:00 -0500

    number table exercise complete

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,15 +1,25 @@
+import java.util.Scanner;
+
 public class ControlFlowExercises {
     public static void main(String[] args) {
-        for (int i = 1; i <= 100; i++) {
-            if (i % 15 == 0) {
-                System.out.println("fizzbuzz");
-            } else if (i % 5 == 0) {
-                System.out.println("buzz");
-            } else if (i % 3 == 0) {
-                System.out.println("fizz");
-            } else {
-                System.out.println(i);
-            }
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = scan.nextInt();
+//        System.out.printf("number | squared | cubed");
+        System.out.println();
+        System.out.println("Here's your table!");
+        System.out.println();
+        System.out.println("number | squared | cubed");
+        System.out.println("------ | ------- | -----");
+        for (int i = 1; i <= number; i++) {
+            System.out.printf("%d      | %-8d| %d", i, i*i, i*i*i);
+//            scan.nextLine();
+            System.out.println();
+        }
+        System.out.print("Do you want to continue? (y/n)");
+        String continueAsk = scan.next();
+        if (continueAsk.equals("y")) {
+            main(null);
         }
     }
 }
