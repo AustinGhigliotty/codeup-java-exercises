0afa6280c8f64c698cfff188d2ec445a8b6ff250
Austin Ghigliotty <austin.ghigliotty@gmail.com>
2019-07-25 16:22:22 -0500

    for loops complete

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,9 +1,10 @@
 public class ControlFlowExercises {
     public static void main(String[] args) {
-        int i = 2;
-        do {
+        for (int i = 5; i <= 15; i++) {
             System.out.println(i);
-            i *= i;
-        }while(i < 65537 && i > 0);
+        }
+        for (long i = 2; i < 1000000; i=i*i) {
+            System.out.println(i);
+        }
     }
 }
